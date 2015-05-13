4853e66e292af8e7542d6824f5f53b430259dede
Corey White <smortopahri@gmail.com>
2015-05-13 11:29:25 -0400

    Added sudo code

---
+++ events.py
@@ -0,0 +1,14 @@
+#Author: Corey White
+#Date: 5/13/15
+#Company: City of Raleighs
+#Description:
+
+import arcpy
+
+def findNewEvents(inFeature, eventTables):
+    #Get event table fields
+    #For each record does
+    #find last event if event exisits
+    #If event
+    #Check if record matches event
+    #Add new event if record does not match event
